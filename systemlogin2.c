c77932b6a70c37664679308ef24e60addc399930
Candidonetrunner <pcandidodev@gmail.com>
2026-08-02 17:17:43 -0300

    adicionei cadastro e senha

---
+++ systemlogin2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+
+char username[10]; 
+char login[10];
+char senha[10];
+char senhav[10];
+char acesso[10];
+int tentativas = 0;
+
+printf("Bem-Vindo\n");
+while (1){
+printf ("digite 'cadastro' para se cadastrar e 'login' para fazer login\n");
+scanf ("%s", acesso);
+if (strcmp(acesso, "cadastro")==0){
+printf ("Escolha seu login e cadastro\n");
+printf("login:");scanf ("%s", login); 
+printf("senha:");scanf ("%s", senhav); 
+}
+else if (strcmp(acesso, "login")==0){
+while(1){
+if (tentativas >= 3){
+printf("limite de tentativas alcançado");
+break;
+}
+printf("login:");scanf("%s", username);
+printf("senha:");scanf("%s", senha);
+if(strcmp(username, login)==0 & strcmp(senha, senhav)==0 ) {
+printf ("acesso liberado\n");
+break;
+}
+else { 
+tentativas += 1; 
+printf("login ou senha incorreto\nrestam %d tentativas\n", 3 - tentativas);
+}
+}
+}
+else {printf("comando invalido");
+}
+}
+}
